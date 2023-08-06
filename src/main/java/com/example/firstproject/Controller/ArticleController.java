db4928edc2ab7ba55a7e4656b17af0f90b6c1658
tariii8653 <ghkd4232@naver.com>
2023-08-07 01:02:45 +0900

    0807

--- src/main/java/com/example/firstproject/Controller/ArticleController.java
+++ src/main/java/com/example/firstproject/Controller/ArticleController.java
@@ -3,11 +3,15 @@ package com.example.firstproject.Controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j //로깅을 위한 어노테이션 추가
 @Controller
 public class ArticleController {
     @Autowired
@@ -18,15 +22,29 @@ public class ArticleController {
     }
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+       log.info(form.toString());
+        //System.out.println(form.toString());
 
         //DTO를 엔티티로 변환
         Article article=form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+        //System.out.println(article.toString());
 
         //레파지토리로 엔티티를 DB에 저장
         Article saved=articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
         return "";
+    }
+    @GetMapping("articles/{id}") //데이터 조회 요청 접수
+    public String show(@PathVariable Long id, Model model){ //매개변수로 id 받아 오기
+        log.info("id=" +id); //id를 잘 받았는지 확인하는 로그 찍기
+
+        //1.id를 조회해 데이터 가져오기: DB에서 데이터를 가져오는 주체는 repository 이다.
+        Article articleEntity=articleRepository.findById(id).orElse(null);
+        //2.모델에 데이터 등록하기 : Model 객체 매개변수에 넣기
+        model.addAttribute("article",articleEntity);
+        //3.뷰 페이지 반환하기
+        return "articles/show";
     }
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor //기본 생성자 추가 어노테이션
+@ToString
 @Entity
 public class Article {
     @Id
@@ -17,18 +23,5 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
