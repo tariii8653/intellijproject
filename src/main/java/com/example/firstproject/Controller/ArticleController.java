d7a485c0092c113c71eca38b850714f11be02f6b
tariii8653 <ghkd4232@naver.com>
2023-08-09 23:48:41 +0900

    0809

--- src/main/java/com/example/firstproject/Controller/ArticleController.java
+++ src/main/java/com/example/firstproject/Controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -85,6 +86,20 @@ public class ArticleController {
         };
         //3.수정 결과 리다이렉트 하기
         return "redirect:/articles/"+articleEntity.getId();
+    }
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제 요청");
+        //1. 삭제할 대상 가져오기
+        Article target=articleRepository.findById(id).orElse(null);
+        //2.대상 엔티티 삭제하기
+        if(target!=null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제되었습니다.");
+        }
+        //3.결과페이지로 리다이렉트 하기
+        return "redirect:/articles";
+
     }
 
 }
