4270fdc4ddac5fd585929f66407d9eae2e0c43a7
tariii8653 <ghkd4232@naver.com>
2023-08-10 19:59:13 +0900

    0810

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into article(id,title,content) values(1,'가가가','111');
-insert into article(id,title,content) values(2,'나나나','222');
-insert into article(id,title,content) values(3,'다다다','333');
+insert into article(title,content) values('가가가','111');
+insert into article(title,content) values('나나나','222');
+insert into article(title,content) values('다다다','333');
