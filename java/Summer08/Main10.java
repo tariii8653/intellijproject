410d86e708013eebc4f6da97abd6736ac1661cdc
tariii8653 <ghkd4232@naver.com>
2023-08-14 15:35:43 +0900

    0814(백준01)

---
+++ java/Summer08/Main10.java
@@ -0,0 +1,10 @@
+package Summer08;
+
+public class Main10 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+    }
+}
---
+++ java/Summer08/Main9.java
@@ -0,0 +1,18 @@
+package Summer08;
+
+import java.util.Scanner;
+
+/**
+ * Main9
+ */
+public class Main9 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long A=sc.nextLong();
+        long B=sc.nextLong();
+        long C=sc.nextLong();
+
+        System.out.println();
+    }
+    
+}
