7761761c54e054424ea00f332e3f302a0e017c8f
tariii8653 <ghkd4232@naver.com>
2023-08-18 22:21:15 +0900

    0818(백준)

--- java/Summer0817/Main2.java
+++ java/Summer0817/Main2.java
@@ -1,33 +1,33 @@
 package Summer0817;
 
 import java.util.Scanner;
-
-
-
-
-//사분면..
+ 
 public class Main2 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-
-        int X=sc.nextInt();
-        int Y=sc.nextInt();
-
-        if(X>0){
-            if(Y>0){
-                System.out.print(1);
-            }
-            else{
-                System.out.print(4);
-            }
-        }
-        else{
-            if(Y>0){
-                System.out.print(2);
-            }
-            else{
-                System.out.print(3);
-            }
-        }
-    } 
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+        
+		int X = in.nextInt();
+		int Y = in.nextInt();
+		
+		if(X > 0) {
+			if(Y > 0) {
+				System.out.print(1);
+			} 
+			else {
+				System.out.print(4);
+			}
+		} 
+		
+		else {
+			if(Y > 0) {
+				System.out.print(2);
+			} 
+			else {
+				System.out.print(3);
+			}
+		}
+	}
+ 
 }
